2f8aeec884ed699db18c373d884a244ed28ebd39
Mangesh <m.sonawanegot@gmail.com>
2025-12-28 19:05:23 +0530

    Issue Resolve

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'public')));
 
     // Handle client-side routing - serve index.html for all non-API routes
-    app.get('*', (req, res) => {
+    app.get(/^\/(?!api).*/, (req, res) => {
         res.sendFile(path.join(__dirname, 'public', 'index.html'));
     });
 } else {
